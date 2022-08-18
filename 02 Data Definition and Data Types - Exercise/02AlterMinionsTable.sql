2bdec28ba90552e3e67fcee292b45c29d13d87a6
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-18 14:28:10 +0300

    Update 02AlterMinionsTable.sql

--- 02 Data Definition and Data Types - Exercise/02AlterMinionsTable.sql
+++ 02 Data Definition and Data Types - Exercise/02AlterMinionsTable.sql
@@ -2,6 +2,6 @@ ALTER TABLE `minions`
 ADD COLUMN `town_id` INT NOT NULL;
 
 ALTER TABLE `minions` 
-ADD CONSTRAINT `fk_id`
+ADD CONSTRAINT `fk_id_m_t`
   FOREIGN KEY (`town_id`)
   REFERENCES `towns` (`id`);
