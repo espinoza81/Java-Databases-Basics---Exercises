7196d453baa486c72560b0ce5f2424e901cec900
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-19 19:32:18 +0300

    Update 11-20.sql

--- 04 Basic CRUD - Exercise/11-20.sql
+++ 04 Basic CRUD - Exercise/11-20.sql
@@ -53,3 +53,8 @@ ORDER BY `hire_date` DESC
 LIMIT 7;
 
 -- 20. Increase Salaries
+UPDATE `employees`
+SET `salary` = `salary` * 1.12
+WHERE `department_id` IN (1, 2, 4, 11);
+
+SELECT `salary` FROM `employees`;
