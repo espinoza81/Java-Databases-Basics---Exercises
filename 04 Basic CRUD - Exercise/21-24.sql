9d48d63f79db939d545029e5f88ec6d2416474de
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-19 19:41:01 +0300

    Update 21-24.sql

--- 04 Basic CRUD - Exercise/21-24.sql
+++ 04 Basic CRUD - Exercise/21-24.sql
@@ -4,5 +4,11 @@ FROM `peaks`
 ORDER BY `peak_name`;
 
 -- 22. Biggest Countries by Population
+SELECT `country_name`, `population`
+FROM `countries`
+WHERE `continent_code` = 'EU'
+ORDER BY `population` DESC, `country_name`
+LIMIT 30;
+
 -- 23. Countries and Currency (Euro / Not Euro)
 -- 24. All Diablo Characters
