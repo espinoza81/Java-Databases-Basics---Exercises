f4c439f904d0e5eac4a65f817331b2293c50c0b3
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-19 16:00:45 +0300

    Update 01.sql

--- 04 Basic CRUD - Exercise/01.sql
+++ 04 Basic CRUD - Exercise/01.sql
@@ -1,8 +1,8 @@
 -- 01. Find All Information About Departments
-SELECT * FROM departments ORDER BY department_id;
+SELECT * FROM `departments` ORDER BY `department_id`;
 
 -- 02. Find all Department Names
-SELECT DISTINCT `name` FROM departments;
+SELECT DISTINCT `name` FROM `departments`;
 
 -- 03. Find Salary of Each Employee;
 SELECT `first_name`, `last_name`, `salary`  FROM `employees`;
@@ -12,12 +12,12 @@ SELECT `first_name`, `middle_name`, `last_name` FROM `employees`;
 
 -- 05. Find Email Address of Each Employee
 SELECT CONCAT(`first_name`, '.', `last_name`, '@softuni.bg') 
-AS full_email_address
+AS `full_email_address`
 FROM `employees`;
 
 -- 06. Find All Different Employee’s Salaries
 SELECT DISTINCT `salary` 
-AS Salary
+AS `Salary`
 FROM `employees`;
 
 -- 07. Find all Information About Employees 
@@ -31,24 +31,24 @@ FROM `employees`
 WHERE `salary` BETWEEN 20000 AND 30000;
 
 -- 09. Find Names of All Employees 
-SELECT CONCAT_WS(' ', first_name, middle_name, last_name)
+SELECT CONCAT_WS(' ', `first_name`, `middle_name`, `last_name`)
 AS 'Full Name'  
 FROM `employees`
 WHERE `salary` IN (25000, 14000, 12500, 23600);
 
 -- 10. Find All Employees Without Manager
-SELECT first_name, last_name 
+SELECT `first_name`, `last_name` 
 FROM `employees`
 WHERE `manager_id` IS NULL;
 
 -- 11. Find All Employees with Salary More Than
-SELECT first_name, last_name, salary 
+SELECT `first_name`, `last_name`, `salary` 
 FROM `employees`
 WHERE `salary` > 50000
 ORDER BY `salary` DESC;
 
 -- 12. Find 5 Best Paid Employees
-SELECT first_name, last_name 
+SELECT `first_name`, `last_name` 
 FROM `employees`
 ORDER BY `salary` DESC
 LIMIT 5;
