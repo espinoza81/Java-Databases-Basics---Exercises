b204f6796fcd09135dceda06152baea2df6f92f8
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-19 19:36:06 +0300

    Update 21-24.sql

--- 04 Basic CRUD - Exercise/21-24.sql
+++ 04 Basic CRUD - Exercise/21-24.sql
@@ -1,4 +1,8 @@
 -- 21. All Mountain Peaks
+SELECT `peak_name` 
+FROM `peaks`
+ORDER BY `peak_name`;
+
 -- 22. Biggest Countries by Population
 -- 23. Countries and Currency (Euro / Not Euro)
 -- 24. All Diablo Characters
