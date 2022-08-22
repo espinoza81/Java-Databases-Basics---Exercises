1cda275e4a69f24d7415cd767b123197841548f0
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 16:04:36 +0300

    Update 07-12.sql

--- 08 Data Aggregation - Exercise/07-12.sql
+++ 08 Data Aggregation - Exercise/07-12.sql
@@ -13,3 +13,19 @@ GROUP BY deposit_group, magic_wand_creator
 ORDER BY magic_wand_creator, deposit_group;
 
 -- 09. Age Groups
+SELECT 
+	CASE
+		WHEN age <= 10 THEN '[0-10]'
+		WHEN age <= 20 THEN '[11-20]'
+		WHEN age <= 30 THEN '[21-30]'
+		WHEN age <= 40 THEN '[31-40]'
+		WHEN age <= 50 THEN '[41-50]'
+		WHEN age <= 60 THEN '[51-60]'
+		ELSE '[61+]'
+	END AS 'age_group',
+	Count(id) AS 'wizard_count'
+FROM wizzard_deposits
+GROUP BY age_group
+ORDER BY wizard_count;
+
+-- 10. First Letter 
