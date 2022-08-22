3cea18e822b597fc2e268f994ba68a195b07130c
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 12:51:42 +0300

    Update 06-10.sql

--- 06 Built-in Functions - Exercise/06-10.sql
+++ 06 Built-in Functions - Exercise/06-10.sql
@@ -5,3 +5,9 @@ WHERE left(`name`, 1) REGEXP 'M|K|B|E'
 ORDER BY `name`;
 
 -- 07. Find Towns Not Starting With
+SELECT * 
+FROM towns
+WHERE left(`name`, 1) NOT REGEXP '[RBD]'
+ORDER BY `name`;
+
+-- 08. Create View Employees Hired After
