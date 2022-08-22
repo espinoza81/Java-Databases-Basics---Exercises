0285ad336c9a0237fcc5b7889e7ed6d4015c9ef0
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 14:48:37 +0300

    Update 11-16.sql

--- 06 Built-in Functions - Exercise/11-16.sql
+++ 06 Built-in Functions - Exercise/11-16.sql
@@ -39,3 +39,7 @@ SELECT `name` AS 'game',
 FROM games;
 
 -- 16. Orders Table 
+SELECT product_name, order_date, 
+ADDDATE(order_date, INTERVAL 3 DAY) AS pay_due, 
+ADDDATE(order_date, INTERVAL 1 MONTH) AS pay_due
+FROM orders;
