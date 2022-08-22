cc06766cbf015c19c8e1145bbe7cd0146e8ea377
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 15:53:26 +0300

    Update 07-12.sql

--- 08 Data Aggregation - Exercise/07-12.sql
+++ 08 Data Aggregation - Exercise/07-12.sql
@@ -7,3 +7,9 @@ HAVING total_sum < 150000
 ORDER BY total_sum DESC;
 
 -- 08. Deposit Charge
+SELECT deposit_group, magic_wand_creator, MIN(deposit_charge) AS 'min_deposit_charge'
+FROM wizzard_deposits
+GROUP BY deposit_group, magic_wand_creator
+ORDER BY magic_wand_creator, deposit_group;
+
+-- 09. Age Groups
