683c5336841484bcb754f4693efb7e13a6edc41a
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 16:15:25 +0300

    Update 07-12.sql

--- 08 Data Aggregation - Exercise/07-12.sql
+++ 08 Data Aggregation - Exercise/07-12.sql
@@ -29,3 +29,10 @@ GROUP BY age_group
 ORDER BY wizard_count;
 
 -- 10. First Letter 
+SELECT left(first_name, 1) AS 'first_letter'
+FROM wizzard_deposits
+WHERE deposit_group = 'Troll Chest'
+GROUP BY first_letter
+ORDER BY first_letter;
+
+-- 11. Average Interest
