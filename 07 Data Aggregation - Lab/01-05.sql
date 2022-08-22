26b8f3007078a478544e3e438a4201722c8f05bc
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 14:58:56 +0300

    Update 01-05.sql

--- 07 Data Aggregation - Lab/01-05.sql
+++ 07 Data Aggregation - Lab/01-05.sql
@@ -5,3 +5,9 @@ GROUP BY department_id
 ORDER BY department_id;
 
 -- 02. Average Salary
+SELECT department_id, ROUND(avg(salary), 2)
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
+
+-- 03. Minimum Salary
