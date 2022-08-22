821205d382fffe80921e9061bae65b01910a989c
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 13:50:37 +0300

    Update 11-16.sql

--- 06 Built-in Functions - Exercise/11-16.sql
+++ 06 Built-in Functions - Exercise/11-16.sql
@@ -1,2 +1,8 @@
 -- 11. Mix of Peak and River Names
+SELECT peaks.peak_name, rivers.river_name, lower(concat(peaks.peak_name, Substring(rivers.river_name, 2))) AS mix    
+FROM peaks  
+INNER JOIN rivers
+WHERE right(peak_name, 1) = Left(river_name, 1)
+ORDER BY mix;
 
+-- 12. Games From 2011 and 2012 Year
