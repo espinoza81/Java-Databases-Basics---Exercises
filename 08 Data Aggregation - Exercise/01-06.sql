b0b9acee00ecc2e149fc0719cf1140825d4066dc
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 15:35:38 +0300

    Update 01-06.sql

--- 08 Data Aggregation - Exercise/01-06.sql
+++ 08 Data Aggregation - Exercise/01-06.sql
@@ -3,3 +3,7 @@ SELECT count(id)
 FROM wizzard_deposits;
 
 -- 02. Longest Magic Wand
+SELECT max(magic_wand_size) AS 'longest_magic_wand'
+FROM wizzard_deposits;
+
+-- 03. Longest Magic Wand per Deposit Groups
