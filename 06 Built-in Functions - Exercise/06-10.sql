e2b9f1e1f6f706f92ecfbd30208e3bacae76e35f
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 13:15:31 +0300

    Update 06-10.sql

--- 06 Built-in Functions - Exercise/06-10.sql
+++ 06 Built-in Functions - Exercise/06-10.sql
@@ -24,3 +24,7 @@ FROM employees
 WHERE length(last_name) = 5;
 
 -- 10. Countries Holding 'A'
+SELECT country_name, iso_code
+FROM countries
+WHERE LENGTH(country_name) - LENGTH(REPLACE(LOWER(country_name), 'a', '')) >= 3
+ORDER BY iso_code;
