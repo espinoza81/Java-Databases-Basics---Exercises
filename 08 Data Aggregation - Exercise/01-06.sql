ae1f7d9aedd3bf61d622d4b7203cb57b471cdad5
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 15:42:43 +0300

    Update 01-06.sql

--- 08 Data Aggregation - Exercise/01-06.sql
+++ 08 Data Aggregation - Exercise/01-06.sql
@@ -13,3 +13,10 @@ GROUP BY deposit_group
 ORDER BY longest_magic_wand, deposit_group;
 
 -- 04. Smallest Deposit Group per Magic Wand Size
+SELECT deposit_group
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY avg(magic_wand_size)
+LIMIT 1;
+
+-- 05. Deposits Sum
