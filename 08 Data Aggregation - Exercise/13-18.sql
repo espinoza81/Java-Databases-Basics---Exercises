31593257b60057ce3905da739dec915b734674cd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 16:58:58 +0300

    Update 13-18.sql

--- 08 Data Aggregation - Exercise/13-18.sql
+++ 08 Data Aggregation - Exercise/13-18.sql
@@ -18,3 +18,10 @@ GROUP BY department_id
 ORDER BY department_id;
 
 -- 14. Employees Maximum Salaries
+SELECT department_id, max(salary) AS max_salary
+FROM employees
+GROUP BY department_id
+HAVING max_salary NOT BETWEEN 30000 AND 70000
+ORDER BY department_id;
+
+-- 15. Employees Count Salaries 
