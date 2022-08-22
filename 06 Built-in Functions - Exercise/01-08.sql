fb77a83b527f3685dc7a8f402145e3784c43353f
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-22 10:57:36 +0300

    Update 01-08.sql

--- 06 Built-in Functions - Exercise/01-08.sql
+++ 06 Built-in Functions - Exercise/01-08.sql
@@ -2,3 +2,10 @@
 SELECT first_name, last_name 
 FROM employees
 WHERE substring(first_name, 1, 2) LIKE 'Sa';
+
+-- 02. Find Names of All Employees by Last Name
+SELECT first_name, last_name 
+FROM employees
+WHERE last_name REGEXP 'ei';
+
+-- 03. Find First Names of All Employess
