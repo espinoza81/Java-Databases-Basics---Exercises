d8a2ba85619045f42a71422745f20358f87ba7e8
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-23 13:50:17 +0300

    Update 01-04.sql

--- 09 Table Relations - Lab/01-04.sql
+++ 09 Table Relations - Lab/01-04.sql
@@ -12,4 +12,10 @@ FOREIGN KEY (mountain_id)
 REFERENCES mountains(id));
    
 -- 02. Trip Organization
-   
+SELECT v.driver_id, v.vehicle_type, concat_ws(' ', d.first_name, d.last_name) AS 'driver_name'
+FROM vehicles AS v
+INNER JOIN campers AS d 
+ON v.driver_id = d.id;
+
+-- 03. SoftUni Hiking
+
