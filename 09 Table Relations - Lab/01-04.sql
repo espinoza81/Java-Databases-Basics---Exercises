d6effb18f70917bcd01881c093d0bd6b9e3727c4
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-23 13:57:26 +0300

    Update 01-04.sql

--- 09 Table Relations - Lab/01-04.sql
+++ 09 Table Relations - Lab/01-04.sql
@@ -18,4 +18,9 @@ INNER JOIN campers AS d
 ON v.driver_id = d.id;
 
 -- 03. SoftUni Hiking
+SELECT r.starting_point, r.end_point, r.leader_id, concat_ws(' ', c.first_name, c.last_name) AS 'leader_name'
+FROM routes AS r
+INNER JOIN campers AS c 
+ON r.leader_id = c.id;
 
+-- 04. Delete Mountains
