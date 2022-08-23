5a8a75f7be40fee058ba55ec750e22102d853c95
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-23 13:40:11 +0300

    Create 01-04.sql

---
+++ 09 Table Relations - Lab/01-04.sql
@@ -0,0 +1,15 @@
+-- 01. Mountains and Peaks
+CREATE TABLE mountains (
+   id INT PRIMARY KEY,
+   name VARCHAR(45));
+   
+CREATE TABLE peaks (
+   id INT PRIMARY KEY,
+   name VARCHAR(45),
+   mountain_id INT,
+   CONSTRAINT fk_p_m
+   FOREIGN KEY (mountain_id)
+   REFERENCES mountains(id));
+   
+   -- 02. Trip Organization
+   
