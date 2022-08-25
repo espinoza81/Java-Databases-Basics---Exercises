05f987544d7fe57b643ad6d9391e030b2686d791
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-25 11:05:58 +0300

    Update 06-11.sql

--- 12 Subqueries and JOINs - Exercise/06-11.sql
+++ 12 Subqueries and JOINs - Exercise/06-11.sql
@@ -7,3 +7,12 @@ WHERE d.`name` IN ('Sales', 'Finance') AND e.hire_date > '1999-01-01 00:00:00'
 ORDER BY e.hire_date;
 
 -- 07. Employees with Project
+SELECT e.employee_id, e.first_name, p.`name`
+FROM employees AS e
+	JOIN employees_projects AS ep USING (employee_id)
+    JOIN projects AS p USING (project_id)
+WHERE DATE(p.start_date) > '2002-08-13' AND p.end_date IS NULL
+ORDER BY e.first_name, p.`name`
+LIMIT 5;
+
+-- 08. Employee 24
