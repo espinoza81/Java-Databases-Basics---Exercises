461238565647a8273516f8dc744f2aa6bc8e5cbb
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-25 13:19:36 +0300

    Update 12-17.sql

--- 12 Subqueries and JOINs - Exercise/12-17.sql
+++ 12 Subqueries and JOINs - Exercise/12-17.sql
@@ -28,3 +28,17 @@ ORDER BY c.country_name
 LIMIT 5;
 
 -- 15. Continents and Currencies
+SELECT continent_code, currency_code, count(currency_code) AS currency_usage
+FROM countries c1
+GROUP BY continent_code, currency_code
+HAVING currency_usage = (
+  SELECT count(currency_code) AS count
+    FROM countries c2
+    WHERE c2.continent_code = c1.continent_code
+    GROUP BY c2.currency_code
+    ORDER BY count DESC
+    LIMIT 1
+  ) AND currency_usage > 1
+ORDER BY continent_code , currency_code;
+
+-- 16. Countries without any Mountains
