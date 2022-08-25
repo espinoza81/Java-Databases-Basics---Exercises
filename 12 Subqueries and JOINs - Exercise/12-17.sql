94271df9eb5d9490d5727b92b174c0236cfaf7f6
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-25 12:56:07 +0300

    Update 12-17.sql

--- 12 Subqueries and JOINs - Exercise/12-17.sql
+++ 12 Subqueries and JOINs - Exercise/12-17.sql
@@ -18,3 +18,13 @@ GROUP BY c.country_code
 ORDER BY mountain_range DESC;
 
 -- 14. Countries with Rivers 
+SELECT c.country_name, r.river_name
+FROM countries AS c 
+LEFT JOIN countries_rivers AS cr USING (country_code)
+LEFT JOIN rivers AS r
+ON cr.river_id = r.id
+WHERE c.continent_code = 'AF'
+ORDER BY c.country_name
+LIMIT 5;
+
+-- 15. Continents and Currencies
