67bb49b67d4f1ca8d85d085c611530c3c830e7dd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-25 10:33:06 +0300

    Update and rename 01-06.sql to 01-05.sql

--- 12 Subqueries and JOINs - Exercise/01-05.sql
+++ 12 Subqueries and JOINs - Exercise/01-05.sql
@@ -25,3 +25,12 @@ WHERE d.`name` = 'Sales'
 ORDER BY e.employee_id DESC;
 
 -- 04. Employee Departments 
+SELECT e. employee_id, e.first_name, e.salary, d.`name`
+FROM employees AS e
+INNER JOIN departments AS d
+ON e.department_id = d.department_id
+WHERE salary > 15000
+ORDER BY d.department_id DESC
+LIMIT 5;
+
+-- 05. Employees Without Project 
