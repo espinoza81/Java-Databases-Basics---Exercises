e03e4c305e670e289dfe6bcec77a4e728244e737
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-25 10:37:35 +0300

    Update 01-05.sql

--- 12 Subqueries and JOINs - Exercise/01-05.sql
+++ 12 Subqueries and JOINs - Exercise/01-05.sql
@@ -34,3 +34,10 @@ ORDER BY d.department_id DESC
 LIMIT 5;
 
 -- 05. Employees Without Project 
+SELECT e.employee_id, e.first_name
+FROM employees AS e
+LEFT JOIN employees_projects AS p
+ON e.employee_id = p.employee_id
+WHERE p.project_id IS NULL
+ORDER BY e.employee_id DESC
+LIMIT 3;
