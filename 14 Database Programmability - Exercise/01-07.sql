272b8e9674ff0fc4623dcffb047b16f0508cf14a
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-26 12:36:55 +0300

    Update 01-07.sql

--- 14 Database Programmability - Exercise/01-07.sql
+++ 14 Database Programmability - Exercise/01-07.sql
@@ -8,3 +8,12 @@ BEGIN
 END
 
 -- 02. Employees with Salary Above Number
+CREATE PROCEDURE usp_get_employees_salary_above(salary_above DECIMAL (11, 4)) 
+BEGIN
+	SELECT first_name, last_name
+	FROM employees
+    WHERE salary >= salary_above
+    ORDER BY first_name, last_name;
+END
+
+-- 03. Town Names Starting With
