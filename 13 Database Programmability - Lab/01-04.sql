3ae35906da2572a7029e968796221d8652ac0d8a
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-26 09:01:40 +0300

    Update 01-04.sql

--- 13 Database Programmability - Lab/01-04.sql
+++ 13 Database Programmability - Lab/01-04.sql
@@ -15,3 +15,12 @@ BEGIN
 END
 
 -- 02. Employees Promotion
+CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR(50)) 
+BEGIN
+	UPDATE employees
+	JOIN departments AS d USING (department_id)
+    SET salary = salary * 1.05
+    WHERE d.`name` = department_name;
+END
+
+-- 03. Employees Promotion By ID
