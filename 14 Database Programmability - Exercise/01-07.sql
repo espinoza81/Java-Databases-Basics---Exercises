b79ca2130f1273574fc52e79f32c6627e7a2cff1
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-26 12:34:17 +0300

    Create 01-07.sql

---
+++ 14 Database Programmability - Exercise/01-07.sql
@@ -0,0 +1,10 @@
+-- 01. Employees with Salary Above 35000 
+CREATE PROCEDURE usp_get_employees_salary_above_35000() 
+BEGIN
+	SELECT first_name, last_name
+	FROM employees
+    WHERE salary > 35000
+    ORDER BY first_name, last_name;
+END
+
+-- 02. Employees with Salary Above Number
