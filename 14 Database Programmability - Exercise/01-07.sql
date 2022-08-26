efd1c44e1bd743e9c329bac64d1c0e8a3eab0635
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-26 15:26:06 +0300

    Update 01-07.sql

--- 14 Database Programmability - Exercise/01-07.sql
+++ 14 Database Programmability - Exercise/01-07.sql
@@ -37,3 +37,16 @@ BEGIN
 END
 
 -- 05. Salary Level Function 
+CREATE FUNCTION ufn_get_salary_level(salary DECIMAL(19, 4))
+RETURNS VARCHAR(8)
+DETERMINISTIC
+BEGIN
+	DECLARE salary_level VARCHAR(8);
+    IF salary < 30000 THEN SET salary_level := 'Low';
+    ELSEIF salary <= 50000 THEN SET salary_level := 'Average';
+	ELSE SET salary_level := 'High';
+    END IF;
+    RETURN salary_level;
+END
+
+-- 06. Employees by Salary Level 
