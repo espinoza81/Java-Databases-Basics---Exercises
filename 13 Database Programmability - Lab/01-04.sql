599b1e7b9ca083b7d1f2bde0b6b189a8207b726a
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-26 09:17:10 +0300

    Update 01-04.sql

--- 13 Database Programmability - Lab/01-04.sql
+++ 13 Database Programmability - Lab/01-04.sql
@@ -24,3 +24,16 @@ BEGIN
 END
 
 -- 03. Employees Promotion By ID
+CREATE PROCEDURE usp_raise_salary_by_id(id INT)  
+BEGIN
+	START TRANSACTION;
+		IF((SELECT count(employee_id) FROM employees WHERE employee_id = id) <> 1) 
+	THEN ROLLBACK;
+    ELSE
+		UPDATE employees
+		SET salary = salary * 1.05
+		WHERE employee_id = id;
+	END IF;
+END
+
+-- 04. Triggered
