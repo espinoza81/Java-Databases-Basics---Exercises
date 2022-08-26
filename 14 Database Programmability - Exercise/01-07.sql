77a9c5335f51a441c5ab7647c5144a14977abf7b
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-26 14:22:12 +0300

    Update 01-07.sql

--- 14 Database Programmability - Exercise/01-07.sql
+++ 14 Database Programmability - Exercise/01-07.sql
@@ -26,3 +26,14 @@ BEGIN
 END
 
 -- 04. Employees from Town 
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR (50)) 
+BEGIN
+    SELECT first_name, last_name
+	FROM employees 
+    JOIN addresses USING (address_id)
+    JOIN towns AS t USING (town_id)
+    WHERE t.`name` LIKE town_name
+    ORDER BY first_name, last_name;
+END
+
+-- 05. Salary Level Function 
