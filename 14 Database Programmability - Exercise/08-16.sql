fdc16bca1d1422d6c9b1b321b5130c4dc8a24466
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-29 09:57:46 +0300

    Update 08-16.sql

--- 14 Database Programmability - Exercise/08-16.sql
+++ 14 Database Programmability - Exercise/08-16.sql
@@ -28,3 +28,22 @@ BEGIN
 END
 
 -- 11. Calculating Interest
+CREATE FUNCTION ufn_calculate_future_value(sum DECIMAL(19, 4), yearly_interest DECIMAL(19, 4), yers INT)
+RETURNS DECIMAL(19, 4)
+DETERMINISTIC
+BEGIN
+	DECLARE future_sum DECIMAL(19, 4);
+    SET future_sum := sum * pow(1 + yearly_interest, yers);
+    RETURN future_sum;
+END;
+
+CREATE PROCEDURE usp_calculate_future_value_for_account(id INT, interest DECIMAL(19, 4)) 
+BEGIN
+	SELECT a.id, first_name, last_name, a.balance AS current_balance, ufn_calculate_future_value(a.balance, interest, 5) AS balance_in_5_years
+	FROM account_holders AS h
+    LEFT JOIN accounts AS a
+    ON h.id = a.account_holder_id
+    WHERE a.id = id;
+END;
+
+-- 12. Deposit Money
