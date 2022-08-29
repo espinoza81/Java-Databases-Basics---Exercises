415092fb9fec7516cfa71742f51d7f95119f73d7
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-29 08:57:21 +0300

    Create 08-16.sql

---
+++ 14 Database Programmability - Exercise/08-16.sql
@@ -0,0 +1,9 @@
+-- 08. Find Full Name
+CREATE PROCEDURE usp_get_holders_full_name () 
+BEGIN
+	SELECT concat_ws(' ', first_name, last_name) AS full_name
+	FROM account_holders
+    ORDER BY full_name;
+END
+
+-- 09. People with Balance Higher Than
