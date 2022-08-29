10df94cf71163a556a5818d9ced4cc6c9b016c60
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-29 13:11:45 +0300

    Update 08-16.sql

--- 14 Database Programmability - Exercise/08-16.sql
+++ 14 Database Programmability - Exercise/08-16.sql
@@ -74,3 +74,25 @@ BEGIN
 END
 
 -- 14. Money Transfer
+CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT , amount DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+		IF (amount <= 0 
+				OR (SELECT balance FROM accounts WHERE id = from_account_id) < amount
+                OR (SELECT count(id) FROM accounts WHERE id = from_account_id) <> 1
+                OR (SELECT count(id) FROM accounts WHERE id = to_account_id) <> 1
+                OR from_account_id = to_account_id) THEN
+    ROLLBACK;
+		ELSE
+			UPDATE accounts
+            SET balance = balance - amount
+            WHERE id = from_account_id;
+		
+			UPDATE accounts
+            SET balance = balance + amount
+            WHERE id = to_account_id;
+            COMMIT;
+        END IF;	
+END
+
+-- 15. Log Accounts Trigger
