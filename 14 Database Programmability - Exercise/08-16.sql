4dc12120b7d8219be3a91fa021c315f0ff0a6a3c
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-08-29 13:44:06 +0300

    Update 08-16.sql

--- 14 Database Programmability - Exercise/08-16.sql
+++ 14 Database Programmability - Exercise/08-16.sql
@@ -96,3 +96,19 @@ BEGIN
 END
 
 -- 15. Log Accounts Trigger
+CREATE TABLE `logs`(
+log_id INT PRIMARY KEY AUTO_INCREMENT, 
+account_id INT NOT NULL,
+old_sum DECIMAL(19, 4) NOT NULL,
+new_sum DECIMAL(19, 4) NOT NULL);
+
+CREATE TRIGGER tr_change_balance
+AFTER UPDATE
+ON accounts
+FOR EACH ROW
+BEGIN
+	INSERT INTO `logs`(account_id, old_sum, new_sum) 
+	VALUES (OLD.id, OLD.balance, NEW.balance);
+END;
+
+-- 16. Emails Trigger
